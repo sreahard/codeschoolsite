c3725c922c31202ef07f56f736fb0f5f83698795
sreahard <suziequz@msn.com>
2015-10-13 16:57:46 -0600

    added a mongo database to my comments

--- server.js
+++ server.js
@@ -1,38 +1,128 @@
-var express = require('express');
-var path = require('path');
-var http = require('http');
-var fs = require('fs');
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/blogComment'); // connect to our database
 
-var app = express();
+var Comment = require('./public/js/comment');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
+var port = process.env.PORT || 4000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:4000/api)
+router.get('/', function(req, res) {
 	res.readFile('index.html')
-})
-
-
-app.post('/api/form', function(req, res) {
-	var newComment = req.body.newComment;
-	var myData = 'Comment: ' + newComment;
-	fs.appendFile('form.json', JSON.stringify(myData), function(err)  {
-		if(err){
-			console.log(err)
-		} else {
-			console.log(JSON.stringify(myData) + ' sucessfully saved');
-			res.redirect('/blog.html');
-		}
-	});
 });
 
-app.get('/api/hello', function(req, res) {
-  res.send("Yo home slice! What's shakin'?")
-});
+// more routes for our API will happen here
+
+// on routes that end in /blogComment
+// ----------------------------------------------------
+router.route('/blogComment')
+
+    // create a comment (accessed at POST http://localhost:4000/api/blogComment)
+    .post(function(req, res) {
+
+        var comment = new Comment();      // create a new instance of the comment model
+        comment.name = req.body.name;  // set the blogComment name (comes from the request)
+
+        // save the comment and check for errors
+        comment.save(function(err) {
+        	if (err)
+        		res.send(err);
+        	res.redirect('/blog.html');
+        	res.json({ message: 'comment ' + comment.name + ' created!' });
+
+        });
+        
+    }) // removed semicolon to link get function
+    // get all the blogComment (accessed at GET http://localhost:4000/api/blogComment)
+    .get(function(req, res) {
+    	Comment.find(function(err, blogComment) {
+    		if (err)
+    			res.send(err);
+
+    		res.json(blogComment);
+    	});
+    });
+
+    // on routes that end in /blogComment/:comment_id
+// ----------------------------------------------------
+router.route('/blogComment/:comment_id')
+
+    // get the comment with that id (accessed at GET http://localhost:4000/api/blogComment/:comment_id)
+    .get(function(req, res) {
+        Comment.findById(req.params.comment_id, function(err, comment) {
+            if (err)
+                res.send(err);
+            res.json(comment);
+        });
+    })
+// update the comment with this id (accessed at PUT http://localhost:4000/api/blogComment/:comment_id)
+    .put(function(req, res) {
+
+        // use our comment model to find the comment we want
+        Comment.findById(req.params.comment_id, function(err, comment) {
+
+            if (err)
+                res.send(err);
+
+            comment.name = req.body.name;  // update the blogComment info
+
+            // save the comment
+            comment.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'comment updated!' });
+            });
+
+        });
+    })
+    
+    // delete the comment with this id (accessed at DELETE http://localhost:4000/api/blogComment/:comment_id)
+    .delete(function(req, res) {
+        Comment.remove({
+            _id: req.params.comment_id
+        }, function(err, comment) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
 
 
 
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api/v1', router);
 
-app.listen(3000);
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
