6630857eff9e5876f7f5df9e8e0e0cf7247efb38
sreahard <suziequz@msn.com>
2015-10-19 14:14:53 -0600

    favico

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
 (function(){
-$.getJSON( "http://localhost:4000/api/v1/blogPosts", function( data ) {
- var items = [];
- $.each( data, function( key, val ) {
-     console.log(key, val)
-     console.log(val.title);
-     console.log(val.body);
-     console.log(val.author);
-     console.log(val.date);
-   items.push( "<h1>" + val.title + "</h1>" + "<p class=\"lead\"> by " + val.author + "</p><hr>" + "<p><span class=\"glyphicon glyphicon-time\"></span>" + " " + val.date + "</p><hr>" + val.body +"</div><hr>" );
- });
- $( "<div/>", {
-   "class": "my-new-list",
-   html: items.join( "" )
- }).appendTo( "#blog-list" );
-});
+	$.getJSON( "/api/v1/blogPosts", function( data ) {
+		var items = [];
+		$.each( data, function( key, val ) {
+			console.log(key, val)
+			console.log(val.title);
+			console.log(val.body);
+			console.log(val.author);
+			console.log(val.date);
+			items.push( "<article><h1>" + val.title + "</h1>" + "<p class=\"lead\"> by " + val.author + "</p><hr>" + "<p><span class=\"glyphicon glyphicon-time\"></span>" + " " + val.date + "</p><hr>" + val.body +"</div><hr></article>" );
+		});
+		$( "<div/>", {
+			"class": "my-new-list",
+			html: items.join( "" )
+		}).appendTo( "#blog-list" );
+	});
 })();
 
 
