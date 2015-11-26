eee6f9827613f63cfb84752bce63bd5b2af67a4b
sreahard <suziequz@msn.com>
2015-11-25 17:08:08 -0700

    trying to fix twitter api for heroku

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.options("*", function(req, res) {
 
 
 var T = new Twit({
-  consumer_key:process.env.CONSUMER_KEY,
+  consumer_key:TeSpTq0uf661OyBu6SaK3oY3f,
   consumer_secret:process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret:process.env.ACCESS_TOKEN_SECRET
